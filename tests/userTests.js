b7bc342f5f48e2d4d41e0213060aef1e88da6f7c
omerugur <ugromer21@gmail.com>
2024-04-19 23:08:58 +0300

    chakram api tesleri

---
+++ tests/userTests.js
@@ -0,0 +1,70 @@
+const chakram = require('chakram'), expect = chakram.expect;
+    
+
+describe("User Tests", function () {
+
+    it("GET /v2/user/{userName}", function () {
+        const response = chakram.get("https://petstore.swagger.io/v2/user/dries34");
+        expect(response).to.have.status(200);
+        return chakram.wait();
+    });
+
+    
+
+    it("POST /v2/user userCreate", function () {
+        const data = {
+            "id": 148971461486,
+            "username": "dries34",
+            "firstName": "dries",
+            "lastName": "mertens",
+            "email": "driesmertens34@gmail.com",
+            "password": "gs1905",
+            "phone": "9816846",
+            "userStatus": 0
+          };
+
+          const resData = {
+            "code": 200,
+            "type": "unknown",
+            "message": "148971461486"
+          };
+
+        const response = chakram.post("https://petstore.swagger.io/v2/user", data);
+        
+        return response.then(function (res){
+            // console.log(res.body);
+            expect(res).to.have.status(200);
+            expect(res).to.have.json(resData);
+        })
+    });
+
+
+    it("PUT /v2/user userUpdate", function () {
+        const data = {
+            "id": 148971461486,
+            "username": "dries34",
+            "firstName": "dries",
+            "lastName": "mertens",
+            "email": "driesmertens34@gmail.com",
+            "password": "gs1905",
+            "phone": "9816846",
+            "userStatus": 0
+          };
+
+        const response = chakram.put("https://petstore.swagger.io/v2/user/dries34", data);
+        
+        return response.then(function (res){
+            
+            expect(res).to.have.status(200);
+        })
+    });
+    
+
+    it("DELETE /v2/user/{username}", function () {
+        const response = chakram.delete("https://petstore.swagger.io/v2/user/dries34");
+        expect(response).to.have.status(200);
+        return chakram.wait();
+    });
+
+
+});
